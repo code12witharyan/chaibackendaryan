dc7ef6b21727d5636fff3716e050b0f2338ab39a
code12witharyan <vidit12jain01@gmail.com>
2025-09-02 12:00:16 +0530

    Cleaned up unnecessary files and added .gitignore

--- index.js
+++ index.js
@@ -15,6 +15,45 @@ const port = 3000
 // })
 
 
+const githubData ={
+     "login": "code12witharyan",
+  "id": 182218601,
+  "node_id": "U_kgDOCtxvaQ",
+  "avatar_url": "https://avatars.githubusercontent.com/u/182218601?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/code12witharyan",
+  "html_url": "https://github.com/code12witharyan",
+  "followers_url": "https://api.github.com/users/code12witharyan/followers",
+  "following_url": "https://api.github.com/users/code12witharyan/following{/other_user}",
+  "gists_url": "https://api.github.com/users/code12witharyan/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/code12witharyan/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/code12witharyan/subscriptions",
+  "organizations_url": "https://api.github.com/users/code12witharyan/orgs",
+  "repos_url": "https://api.github.com/users/code12witharyan/repos",
+  "events_url": "https://api.github.com/users/code12witharyan/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/code12witharyan/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Aryan Sanjay Jain",
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 3,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 0,
+  "created_at": "2024-09-20T09:50:54Z",
+  "updated_at": "2025-01-03T07:40:50Z"
+}
+
+app.get('/github',(req,res)=>{
+    res.json(githubData)
+})
 
 app.get('/twitter',(req,res)=>{
     res.send('This is my twitter handle https://twitter.com/viditjain2002')
